83485942871a40edd01f0e9b30b120e2b9f7637b
jackgle <jglebien@gmail.com>
2017-05-30 20:56:45 -0500

    Fifth commit

--- test/run_patterndetect.m
+++ test/run_patterndetect.m
@@ -1,4 +1,4 @@
 % Run pattern detection and extraction
-cs = click_extract(signal,target,-105,401,time,'test_file');
+cs = click_extract(signal,target,-103,401,time,'test_file');
 % Plot and highlight extracted frames
 plot_extracts(cs,signal,time);
