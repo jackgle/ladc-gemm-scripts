8f7e88faa77cd852b9442be8f6cb540858610ca4
jackgle <jglebien@gmail.com>
2017-06-02 15:02:35 -0500

    move files

--- centroids.m
+++
@@ -1,7 +0,0 @@
-function [C] = centroids(fmat,idx)
-    
-k = length(unique(idx));
-C = zeros(k,size(fmat,2));
-for i = 1:k
-    C(i,:) = mean(fmat(idx==i,:));
-end
--- play_clicks.m
+++
@@ -1,7 +0,0 @@
-Fs = 192000;
-
-fields = fieldnames(click_struct);
-for i = 1:size(fields,1)
-  soundsc(click_struct.(fields{i}).sig, Fs*0.01);
-  pause(1);
-end
