49721f7a933b4caac2e43b2636f7f78ebb24dfa3
jackgle <jglebien@gmail.com>
2017-06-12 18:28:39 -0500

    fix play_clicks

--- tools/play_clicks.m
+++ tools/play_clicks.m
@@ -1,7 +1,7 @@
+function play_clicks(cstruct)
 Fs = 192000;
 
-fields = fieldnames(click_struct);
-for i = 1:size(fields,1)
-  soundsc(click_struct.(fields{i}).sig, Fs*0.01);
+for i = 1:length(cstruct)
+  soundsc(cstruct(i).sig, Fs*0.01);
   pause(1);
 end
