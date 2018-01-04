cd48ff5e3f5b7f16474d07e471b9138f87be0eef
Jack <jackgle@Jacks-MacBook-Pro.local>
2018-01-03 21:34:07 -0600

    refactor repository

--- tools/play_clicks.m
+++
@@ -1,7 +0,0 @@
-function play_clicks(cstruct)
-Fs = 192000;
-
-for i = 1:length(cstruct)
-  soundsc(cstruct(i).sig, Fs*0.01);
-  pause(1);
-end
--- tools/shannon_entropy.m
+++
@@ -1,9 +0,0 @@
-function [entropy] = shannon_entropy(x)
-
-% x = interp(x,5);
-x=double(x(:));
-% nbins = round(range(x)/((2*iqr(x)/nthroot(length(x),3)))); % Freedman-Diaconis
-p = hist(x,50);
-p = p/sum(p);
-
-entropy = -sum(p.*log2(p+eps));
